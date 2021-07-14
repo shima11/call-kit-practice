195bd2b8c7825eef3b17a5c6bccbc01b00d115dd
Shima <shima.jin@icloud.com>
2021-07-14 15:04:58 +0900

    update

--- call-kit-practice/ViewController.swift
+++ call-kit-practice/ViewController.swift
@@ -11,20 +11,32 @@ import CallKit
 
 final class ViewController: UIViewController {
 
+  let statusLabel = UILabel()
+
   override func viewDidLoad() {
+
     super.viewDidLoad()
 
     view.backgroundColor = .white
 
+    view.addSubview(statusLabel)
+    statusLabel.easy.layout(
+      Center()
+    )
+
+    statusLabel.text = "..."
+    statusLabel.font = UIFont.boldSystemFont(ofSize: 20)
+    statusLabel.textColor = .darkText
+
     do {
-      let addButton = UIButton(type: .contactAdd)
+      let addButton = UIButton(type: .close)
       view.addSubview(addButton)
       addButton.easy.layout(Left(48), Bottom(80))
       addButton.addTarget(self, action: #selector(startCall), for: .touchUpInside)
     }
 
     do {
-      let addButton = UIButton(type: .close)
+      let addButton = UIButton(type: .contactAdd)
       view.addSubview(addButton)
       addButton.easy.layout(Right(48), Bottom(80))
       addButton.addTarget(self, action: #selector(endCall), for: .touchUpInside)
@@ -32,11 +44,15 @@ final class ViewController: UIViewController {
   }
 
   @objc func startCall() {
+    print("start call")
 
+    statusLabel.text = "start call"
   }
 
   @objc func endCall() {
+    print("end call")
 
+    statusLabel.text = "end call"
   }
 
 }
